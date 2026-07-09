fb9ac661f6842fc613b842577a636f8d8885fe71
Nikita <nikit4684@gmail.com>
2026-07-09 13:08:22 +0300

    Displaying Variable Memory Addresses in C
    This program demonstrates how to obtain and display the memory addresses of variables using the address-of operator (&) in C. It prints the locations of several integer variables, introducing the concept of memory layout and variable storage. The example helps beginners understand how variables are stored in memory and serves as an introduction to pointers and memory management concepts in C programming.

---
+++ PR - 1.3.2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+    int a2, a4, a1, a3;
+    printf ("Address of a1 = %X \n", &a1 );
+    printf ("Address of a2 = %X \n", &a2 );
+    printf ("Address of a3 = %X \n", &a3 );
+    printf ("Address of a4 = %X \n", &a4 );
+    return 0;
+}
